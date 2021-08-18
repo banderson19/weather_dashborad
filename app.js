87aa45e03749efe6a1ca9094100813af2d006cb1
ogMav8 <brad.anderson019@gmail.com>
2021-08-18 12:35:26 -0600

    looped through 5 day forcast

--- app.js
+++ app.js
@@ -37,5 +37,48 @@ const getWeather = () => {
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        
+        cityName = $("<h3>").text(data.city.name);
+        $("#today-weather").append(cityName);
+
+        // date = $("h3>").text(data.list[0].dt_text);
+        // console.log(date)
+        // $("today-weather").append(date)
+
+        var TempetureToNum = parseInt((data.list[0].main.temp)* 9/5 - 459);
+        var cityTemperature = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
+        $("#today-weather").append(cityTemperature);
+        var cityHumidity = $("<p>").text("Humidity: "+ data.list[0].main.humidity + " %");
+        $("#today-weather").append(cityHumidity);
+        var cityWindSpeed = $("<p>").text("Wind Speed: "+ data.list[0].wind.speed + " MPH");
+        $("#today-weather").append(cityWindSpeed);
+
+
+        for (var i=1; i<6; i++) {
+          var TempToNum = parseInt((data.list[i].main.temp)* 9/5 -459);
+          var cityTemp = $("<p>").text("Temp: " + TempToNum + " °F")
+          $("#five-day").append(cityTemp);
+          var cityHum = $("<p>").text("Humidity: " + data.list[i].main.humidity + " %");
+          $("#five-day").append(cityHum);
+          var windSpeed = $("<p>").text("Wind Speed: " + data.list[i].wind.speed + " MPH");
+          $("#five-day").append(windSpeed);
+        }
+        
+        // var TempetureToNum_1 = parseInt((data.list[1].main.temp)* 9/5 - 459);
+        // var cityTemperature_1 = $("<p>").text("Temp: "+ TempetureToNum_1 + " °F");
+        // $("#five-day-1").append(cityTemperature_1);
+        // var cityHumidity_1 = $("<p>").text("Humidity: "+ data.list[1].main.humidity + " %");
+        // $("#five-day-1").append(cityHumidity_1);
+        // var cityWindSpeed = $("<p>").text("Wind Speed: "+ data.list[1].wind.speed + " MPH");
+        // $("#five-day-1").append(cityWindSpeed);
+
+        // var TempetureToNum_2 = parseInt((data.list[2].main.temp)* 9/5 - 459);
+        // var cityTemperature_2 = $("<p>").text("Temp: "+ TempetureToNum_2 + " °F");
+        // $("#five-day-2").append(cityTemperature_2);
+        // var cityHumidity_2 = $("<p>").text("Humidity: "+ data.list[2].main.humidity + " %");
+        // $("#five-day-2").append(cityHumidity_2);
+        // var cityWindSpeed = $("<p>").text("Wind Speed: "+ data.list[2].wind.speed + " MPH");
+        // $("#five-day-2").append(cityWindSpeed);
+
     })
 }
